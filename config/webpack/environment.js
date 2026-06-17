970e411c4aa0b90a9ba27347eb317dc515c8546f
Lionel Luchez <lluchez@users.noreply.github.com>
2026-06-17 10:59:44 -0500

    Yarn CVE upgrades and Upgrade to Node 22 (#105)
    * Fix Yarn CVEs
    
    * More changes
    
    * Upgrade to Node 22

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,10 @@
 const { environment } = require('@rails/webpacker')
 
+// Webpack 4 defaults to md4, which is not available with modern OpenSSL in Node 17+.
+environment.config.merge({
+	output: {
+		hashFunction: 'sha256'
+	}
+})
+
 module.exports = environment
